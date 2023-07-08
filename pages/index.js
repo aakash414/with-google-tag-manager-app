20dda1de55c90befed45fc13c687d561f0b6ff80
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-07-08 19:51:22 +0530

    working

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,29 @@ export default function Home() {
         Go to `pages/_app.js` and `pages/_document.js` to see how you can add
         Google Tag Manager to your app
       </h1>
+      <form action="#" method="POST">
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          required
+        ></input>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        ></input>
+        <textarea
+          placeholder="Enter message"
+          name="message"
+          required
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
+      <button type="button" id="new button">
+        New
+      </button>
     </div>
-  )
+  );
 }
