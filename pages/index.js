7dc1410dd1407d8e1e17253c7874003a3396ad79
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-07-09 18:54:16 +0530

    second form

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
         Go to `pages/_app.js` and `pages/_document.js` to see how you can add
         Google Tag Manager to your app
       </h1>
-      <form action="#" method="POST">
+      <form name="first" action="#firstform" method="POST">
         <input
           type="text"
           name="name"
@@ -25,9 +25,31 @@ export default function Home() {
         ></textarea>
         <button type="submit">Send</button>
       </form>
-      <button type="button" id="new button">
-        New
-      </button>
+      <a href="www.google.com" target="blank">
+        <button type="submit" id="new button">
+          New
+        </button>
+      </a>
+      <form name="second" action="#html" method="POST">
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          required
+        ></input>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        ></input>
+        <textarea
+          placeholder="Enter message"
+          name="message"
+          required
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
